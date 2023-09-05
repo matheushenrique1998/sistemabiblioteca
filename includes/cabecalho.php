0d4eefe7ee373e79831d492f710e6741e1219ec0
matheushenrique1998 <134648358+matheushenrique1998@users.noreply.github.com>
2023-09-05 16:05:20 -0300

    pa

---
+++ includes/cabecalho.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>sistema Biblioteca</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <style>
+        :root{
+            --bs-primary-rgb:247,148,30;
+            --bs-secondary-rgb:0,74,141;
+        }
+    </style>
+</head>
+
+<body class="d-flex flex-column min-vh-100">
+    <header>
+        <nav class="navbar navbar-expand-lg bg-primary"data-bs-theme="dark">
+            <div class="container">
+                <a class="navbar-brand" href="#">Biblioteca</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link" href="#">Emprestimos</a>
+                        <a class="nav-link" href="#">Livros</a>
+                        <a class="nav-link" href="#">Alunos</a>
+                        <a class="nav-link" href="#">Usuários</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
