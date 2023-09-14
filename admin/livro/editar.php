032f0fbb40f95206b9adf04032f7b883603eb970
matheushenrique1998 <134648358+matheushenrique1998@users.noreply.github.com>
2023-09-14 17:18:54 -0300

    po

--- admin/livro/editar.php
+++ admin/livro/editar.php
@@ -1,25 +1,50 @@
 <?php
 
 require_once $_SERVER['DOCUMENT_ROOT'] . "/includes/cabecalho.php";
-require_once $_SERVER['DOCUMENT_ROOT'] . "/controllers/UsuarioController.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/controllers/LivroController.php";
 
-$livroController= new livroController();
-$livro=$livroController->editarUlivro();
+$LivroController= new LivroController();
+$livro=$LivroController->editarlivro();
 //var_dump($uorsuario);
 ?>
 <main class="container mt-3 mb-3">
     <h1>editar livro</h1>
         <form action="editar.php?id_usuario=<?=$livro->id_livro?>"  method="POST"  class="row g-3">
     
-  <div class="col-md-12">
-    <label for="nome" class="form-label"> titulo</label>
-    <input type="text" name="titulo" id="titulo"
-     class="form-control" required value="<?=$livro>titulo ?>">
+        <div class="col-md-12">
+    <label for="nome" class="form-label">titulo</label>
+    <input type="text" name="titulo" id="titulo" 
+    class="form-control" required value="<?=$livro->titulo?>">
 </div>
 <div class="col-md-6">
     <label for="nome" class="form-label">autor</label>
     <input type="text" name="autor" id="autor" 
     class="form-control" required value="<?=$livro->autor?>">
+</div>
+<div class="col-md-6">
+    <label for="nome" class="form-label">autor</label>
+    <input type="text" name="autor" id="autor" 
+    class="form-control" required value="<?=$livro->autor?>">
+</div>
+<div class="col-md-6">
+    <label for="nome" class="form-label">numero_pagina</label>
+    <input type="text" name="numero_pagina" id="numero_pagina" 
+    class="form-control" required value="<?=$livro->numero_pagina?>">
+</div>
+<div class="col-md-6">
+    <label for="nome" class="form-label">perco</label>
+    <input type="text" name="perco" id="perco" 
+    class="form-control" required value="<?=$livro->perco?>">
+</div>
+<div class="col-md-6">
+    <label for="nome" class="form-label">isbn</label>
+    <input type="text" name="isbn" id="isbn" 
+    class="form-control" required value="<?=$livro->isbn?>">
+</div>
+<div class="col-md-6">
+    <label for="nome" class="form-label">ano_publicacao</label>
+    <input type="text" name="ano_publicacao" id="ano_publicacao" 
+    class="form-control" required value="<?=$livro->ano_publicacao?>">
 </div>
 
     <div class="col-12">
@@ -30,3 +55,9 @@ $livro=$livroController->editarUlivro();
 </form>
 
 </main>
+
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . "/includes/rodape.php";
+
+?>
--- controllers/LivroController.php
+++ controllers/LivroController.php
@@ -1,7 +1,7 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . "/models/livro.php";
 
-class LivroCotroller{
+class LivroController{
     private $livroModel;
     public function __construct(){
         
@@ -9,7 +9,39 @@ class LivroCotroller{
     
  }
  
- public function listarlivros(){
+ public function listarlivro(){
     return $this->livroModel->listar();
  }
+ public function editarlivro($id_livro){
+   $id_usuario =$_GET['id_livro'];
+   if($_SERVER['REQUEST_METHOD']== 'POST'){
+     if(isset($_POST['senha'])&& !empty($_POST['senha'])){
+      //criar nova senha
+      $senha = password_hash($_POST['senha'],PASSWORD_DEFAULT);
+     }else{
+      //manter senha antiga
+      $id_livro= $this->livroModel->buscar($id_livro);
+      $senha= $id_livro->senha;
+
+     }
+      $dados=[
+         'titulo'=> $_POST['titulo'],
+         'autor'=> $_POST['autor'],
+         'numero_pagina'=> $_POST['numero_pagina'],
+         'perco'=> $_POST['perco'],
+         'isbn'=> $_POST['isbn'],
+         'ano_publicacao'=> $_POST['ano_publicacao']
+      ];
+      $this->livroModel->editar($id_livro,$dados);
+      header('location: index.php');
+      exit;
+   }
+
+   return $this->livroModel->buscar($id_livro);
+}
+public function excluirlivro(){
+   $this->livroModel->excluir($_GET['id_usuario']);
+   header('location: index.php');
+   exit;
+}
 }
